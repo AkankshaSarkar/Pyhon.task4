69e09591a268b7c9e6f5347d604522323ee6d81c
Akanksha Sarkar <akankshasarkar4@gmail.com>
2026-02-23 11:31:00 +0530

    Add files via upload

---
+++ handle error.py
@@ -0,0 +1,11 @@
+try:
+    print("Reading file content:")
+
+    with open("../../../PyCharmMiscProject/sample.txt", "r") as file:
+        lines = file.readlines()
+
+        for i, line in enumerate(lines, start=1):
+            print(f"Line {i}: {line.strip()}")
+
+except FileNotFoundError:
+    print("Error: The file 'sample.txt' was not found.")
